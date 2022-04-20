2de516f8b4e3c1a417f61724a659d4bbcb2513bc
xqb64 <x@xqb.ch>
2022-04-20 20:06:28 +0200

    feat: simple REPL

--- src/main.c
+++ src/main.c
@@ -1,20 +1,38 @@
+#include <stdio.h>
+#include <stdlib.h>
 #include "compiler.h"
 #include "tokenizer.h"
 #include "parser.h"
 #include "vm.h"
 
-int main(int argc, char *argv[]) {
-    char *source = "print 6 * 4;";
+void repl() {
     Tokenizer tokenizer;
     Parser parser;
     BytecodeChunk chunk;
     VM vm;
-    init_tokenizer(&tokenizer, source);
+
     init_vm(&vm);
-    Statement stmt = parse(&parser, &tokenizer);
-    compile(&chunk, &vm, stmt);
-    run(&vm, &chunk);
+    for (;;) {
+        size_t len = 0;
+        char *line = NULL;
+
+        printf("> ");
+
+        if (getline(&line, &len, stdin) == EOF) exit(0); 
+        
+        init_tokenizer(&tokenizer, line);
+        Statement stmt = parse(&parser, &tokenizer);
+        compile(&chunk, &vm, stmt);
+        run(&vm, &chunk);
+
+        free_ast(stmt.exp.data.binexp);
+        free(line);
+    }
+
     free_vm(&vm);
     free_chunk(&chunk);
-    free_ast(stmt.exp.data.binexp);
+}
+
+int main(int argc, char *argv[]) {
+    if (argc == 1) repl();
 }
