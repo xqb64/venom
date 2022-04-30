6732efdecdc829be1bb3676cfb98fb940fc161bc
xqb64 <x@xqb.ch>
2022-04-30 20:17:08 +0200

    refactor: tweak tests

--- tests/test_calculator.py
+++ tests/test_calculator.py
@@ -3,60 +3,76 @@ import subprocess
 import pytest
 
 @pytest.mark.parametrize(
-    "a, op, b, expected",
+    "a, b",
     [   # 1-digit operands ops
-        [2,  "+", 2,  "4.00"],
-        [2,  "-", 2,  "0.00"],
-        [2,  "*", 2,  "4.00"],
-        [4,  "/", 2,  "2.00"],
-        [2,  "/", 4,  "0.50"],
+        [2, 2],
+        [4, 2],
+        [2, 4],
         # 2-digit operands ops
-        [3,  "+", 10, "13.00"],
-        [10, "+", 3,  "13.00"],
-        [10, "+", 10, "20.00"],
-        [3,  "-", 10, "-7.00"],
-        [10, "-", 3,  "7.00"],
-        [10, "-", 10, "0.00"],
-        [3,  "*", 10, "30.00"],
-        [10, "*", 3,  "30.00"],
-        [10, "/", 2,  "5.00"],
-        [2,  "/", 10, "0.20"],
+        [3, 10],
+        [10, 3],
+        [10, 10],
         # negative operands ops
-        [2,  "+", -2, "0.00"],
-        [-2, "+", 2,  "0.00"],
-        [-2, "+", -2, "-4.00"],
-        [-2, "-", 2,  "-4.00"],
-        [2,  "-", -2,  "4.00"],
-        [-2, "-", -2,  "0.00"],
+        [2, -2],
+        [-2, 2],
+        [-2, -2],
+        [-2, 2],
     ]
 )
-def test_calculator(a, op, b, expected):
-    source = textwrap.dedent(
-        f"""\
-        print {a} {op} {b};"""
-    )
-    output = subprocess.check_output(["./a.out"], input=source.encode('utf-8'))
-    assert "dbg print :: {}\n".format(expected).encode('utf-8') in output
+def test_calculator(a, b):
+    for op in {'+', '-', '*', '/'}:
+        source = textwrap.dedent(
+            f"""\
+            print {a} {op} {b};"""
+        )
+        match op:
+            case '+': 
+                expected = "%.2f" % (a + b)
+            case '-': 
+                expected = "%.2f" % (a - b)
+            case '*': 
+                expected = "%.2f" % (a * b)
+            case '/': 
+                expected = "%.2f" % (a / b)
+        output = subprocess.check_output(["./a.out"], input=source.encode('utf-8'))
+        assert "dbg print :: {}\n".format(expected).encode('utf-8') in output
 
 
 @pytest.mark.parametrize(
-    "a, op, b, op2, c, expected",
+    "a, b, c",
     [
-        [2, "*", 3, "*", 4, "24.00"],
-        [2, "+", 3, "*", 4, "20.00"],
-        [2, "-", 3, "*", 4, "-4.00"],
-        [6, "/", 3, "+", 4, "6.00"],
-        [6, "/", 3, "/", 2, "1.00"],
-        [6, "-", 3, "/", 2, "1.50"],
+        [2, 3, 4],
+        [-1, 2, 3],
+        [4, -5, 6],
+        [7, 8, -9],
+        [10, 8, 9],
+        [-10, 8, 9],
+        [10, -8, 9],
+        [10, 8, -9],
     ]
 )
-def test_calculator_grouping(a, op, b, op2, c, expected):
-    source = textwrap.dedent(
-        f"""\
-        print ({a} {op} {b}) {op2} {c};"""
-    )
-    output = subprocess.check_output(["./a.out"], input=source.encode('utf-8'))
-    assert "dbg print :: {}\n".format(expected).encode('utf-8') in output
+def test_calculator_grouping(a, b, c):
+    for op in {'+', '-', '*', '/'}:
+        for op2 in {'+', '-', '*', '/'}:
+            source = textwrap.dedent(
+                f"""\
+                print ({a} {op} {b}) {op2} {c};"""
+            )
+
+            match op:
+                case '+': in_parens = (a + b)
+                case '-': in_parens = (a - b)
+                case '*': in_parens = (a * b)
+                case '/': in_parens = (a / b)
+
+            match op2:
+                case '+': expected = "%.2f" % (in_parens + c)
+                case '-': expected = "%.2f" % (in_parens - c)
+                case '*': expected = "%.2f" % (in_parens * c)
+                case '/': expected = "%.2f" % (in_parens / c)
+            
+            output = subprocess.check_output(["./a.out"], input=source.encode('utf-8'))
+            assert "dbg print :: {}\n".format(expected).encode('utf-8') in output
 
 def test_calculator_grouping_nested():
     source = "print (((6 + (4 * 2)) - 4) / 2);"
--- tests/test_declaration.py
+++ tests/test_declaration.py
@@ -3,62 +3,52 @@ import subprocess
 import pytest
 
 @pytest.mark.parametrize(
-    "value, expected",
-    [
-        [1, "1.00"],
-        [-1, "-1.00"],
-        [23, "23.00"],
-        [-23, "-23.00"],
-        [3.14, "3.14"],
-        [-3.14, "-3.14"],
-        [0, "0.00"],
-        [100, "100.00"],
-        [-100, "-100.00"],
-    ]
+    "value",
+    [1, -1, 23, -23, 3.14, -3.14, 0, 100, -100],
 )
-def test_declarations(value, expected):
+def test_declarations(value):
     source = textwrap.dedent(
         f"""\
         let x = {value};
         print x;"""
     )
+    expected = '%.2f' % value
     output = subprocess.check_output(["./a.out"], input=source.encode('utf-8'))
     assert "dbg print :: {}\n".format(expected).encode('utf-8') in output
 
 
 @pytest.mark.parametrize(
-    "a, op, b, expected",
+    "a, b",
     [   # 1-digit operands ops
-        [2,  "+", 2,  "4.00"],
-        [2,  "-", 2,  "0.00"],
-        [2,  "*", 2,  "4.00"],
-        [4,  "/", 2,  "2.00"],
-        [2,  "/", 4,  "0.50"],
+        [2, 2],
+        [4, 2],
+        [2, 4],
         # 2-digit operands ops
-        [3,  "+", 10, "13.00"],
-        [10, "+", 3,  "13.00"],
-        [10, "+", 10, "20.00"],
-        [3,  "-", 10, "-7.00"],
-        [10, "-", 3,  "7.00"],
-        [10, "-", 10, "0.00"],
-        [3,  "*", 10, "30.00"],
-        [10, "*", 3,  "30.00"],
-        [10, "/", 2,  "5.00"],
-        [2,  "/", 10, "0.20"],
+        [3, 10],
+        [10, 3],
+        [10, 10],
         # negative operands ops
-        [2,  "+", -2, "0.00"],
-        [-2, "+", 2,  "0.00"],
-        [-2, "+", -2, "-4.00"],
-        [-2, "-", 2,  "-4.00"],
-        [2,  "-", -2,  "4.00"],
-        [-2, "-", -2,  "0.00"],
+        [2, -2],
+        [-2, 2],
+        [-2, -2],
+        [-2, 2],
     ]
 )
-def test_declarations_with_expressions(a, op, b, expected):
-    source = textwrap.dedent(
-        f"""\
-        let x = {a} {op} {b};
-        print x;"""
-    )
-    output = subprocess.check_output(["./a.out"], input=source.encode('utf-8'))
-    assert "dbg print :: {}\n".format(expected).encode('utf-8') in output
+def test_declarations_with_expressions(a, b):
+    for op in {'+', '-', '*', '/'}:
+        source = textwrap.dedent(
+            f"""\
+            let x = {a} {op} {b};
+            print x;"""
+        )
+        match op:
+            case '+': 
+                expected = "%.2f" % (a + b)
+            case '-': 
+                expected = "%.2f" % (a - b)
+            case '*': 
+                expected = "%.2f" % (a * b)
+            case '/': 
+                expected = "%.2f" % (a / b)
+        output = subprocess.check_output(["./a.out"], input=source.encode('utf-8'))
+        assert "dbg print :: {}\n".format(expected).encode('utf-8') in output
