e9f13b159dbc53a833e8e5dec57ee6e526b62205
xqb64 <x@xqb.ch>
2022-05-09 04:40:46 +0200

    refactor: tweak a test

--- tests/test_negate.py
+++ tests/test_negate.py
@@ -5,24 +5,29 @@ import textwrap
 from tests.util import VALGRIND_CMD
 from tests.util import SINGLE_OPERAND_GROUP
 
+
 @pytest.mark.parametrize(
     "x",
     SINGLE_OPERAND_GROUP,
 )
 def test_assignment(x):
-    for i in range(10):
+    for minus_count in range(10):
         source = textwrap.dedent(
             f"""\
-            let x = {'-' * i + str(x)};
+            let x = {'-' * minus_count + str(x)};
             print x;
             """
         )
         
-        process = subprocess.run(VALGRIND_CMD,
+        process = subprocess.run(
+            VALGRIND_CMD,
             capture_output=True,
             input=source.encode('utf-8')
         )
-        assert (f"dbg print :: " + 
-            f"{'-' if (x == 0 and i % 2 != 0) else ''}" + 
-            f"{eval('-' * i + str(x)):.2f}\n").encode('utf-8') in process.stdout
+
+        assert ("dbg print :: " + 
+            # for x == 0 and for every odd number of minuses,
+            # the result is going to have a minus in front. 
+            f"{'-' if (x == 0 and minus_count % 2 != 0) else ''}" +
+            f"{eval('-' * minus_count + str(x)):.2f}\n").encode('utf-8') in process.stdout
         assert process.returncode == 0
