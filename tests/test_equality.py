9abe496247be23f8768bdeb6b543404c2271f8dd
xqb64 <x@xqb.ch>
2023-01-14 05:44:49 +0100

    write a struct equality test

--- tests/test_equality.py
+++ tests/test_equality.py
@@ -65,3 +65,49 @@ def test_equality_func(tmp_path, x, y):
     
     # null must remain on the stack because it's a void func
     assert f"stack: [null]".encode('utf-8') in process.stdout
+
+
+def test_equality_two_structs(tmp_path):
+    source = textwrap.dedent(
+        """
+        struct spam {
+            x;
+            y;
+        }
+
+        fn main() {
+            let a = %s;
+            let b = %s;
+            print a == b;
+        }
+        
+        main();"""
+    )
+
+    structs = [
+        ("spam { x: 5, y: 10 }", "spam { x: 5, y: 10 }", True),
+        ("spam { x: 5, y: 10 }", "spam { x: 5, y: 0 }", False),
+        ('spam { x: 5, y: "Hello, world!" }', 'spam { x: 5, y: "Hello, world!" }', True),
+        ('spam { x: 5, y: "Hello, world!" }', 'spam { x: 5, y: "Bye there!" }', False),
+        ('spam { x: 5, y: spam { x: 3, y: 6 } }', 'spam { x: 5, y: spam { x: 3, y: 6 } }', True),
+        ('spam { x: 5, y: spam { x: 3, y: 6 } }', 'spam { x: 5, y: spam { x: 12, y: 6 } }', False),
+        ("spam { x: 5, y: 10 }", 'spam { x: "Hello, world!", y: 10 }', False),
+    ]
+
+    for a, b, is_equal in structs:
+        input_file = tmp_path / "input.vnm"
+        input_file.write_text(source % (a, b))
+        
+        process = subprocess.run(
+            VALGRIND_CMD + [input_file],
+            capture_output=True,
+        )
+
+        expected = 'true' if is_equal else 'false'
+
+        assert f"dbg print :: {expected}\n".encode('utf-8') in process.stdout
+        assert process.returncode == 0
+        
+        # null must remain on the stack because it's a void func
+        assert f"stack: [null]".encode('utf-8') in process.stdout
+
