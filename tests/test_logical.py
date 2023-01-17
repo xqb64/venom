6a53a3b0389ea8d232b071107c7121a907db67f9
xqb64 <x@xqb.ch>
2023-01-17 03:33:48 +0100

    add a test that exercises logical operators

---
+++ tests/test_logical.py
@@ -0,0 +1,53 @@
+import subprocess
+import pytest
+import textwrap
+
+from tests.util import VALGRIND_CMD
+
+
+@pytest.mark.parametrize(
+    "a, b, if_a_equals, op, if_b_equals, should_print",
+    [
+        # Logical AND
+        (32, 64, 32, "&&", 64, "Run!"),
+        (32, 64, 16, "&&", 64, "Shouldn't run!"),
+        (32, 64, 32, "&&", 16, "Shouldn't run!"),
+        (32, 64, 128, "&&", 256, "Shouldn't run!"),
+
+        # Logical OR
+        (32, 64, 32, "||", 64, "Run!"),
+        (32, 64, 16, "||", 64, "Run!"),
+        (32, 64, 32, "||", 16, "Run!"),
+        (32, 64, 128, "||", 256, "Shouldn't run!"),
+    ],
+)
+def test_logical(tmp_path, a, b, if_a_equals, op, if_b_equals, should_print):
+    source = textwrap.dedent(
+        """\
+        fn main() {
+          let x = %s;
+          let y = %s;
+          if (x == %s %s y == %s) {
+            print "Run!";
+          } else {
+            print "Shouldn't run!";
+          }
+        }
+        main();
+        """
+    )
+    input_file = tmp_path / "input.vnm"
+    input_file.write_text(source % (a, b, if_a_equals, op, if_b_equals))
+    
+    process = subprocess.run(
+        VALGRIND_CMD + [input_file],
+        capture_output=True,
+    )
+
+    expected = should_print
+
+    assert f"dbg print :: {expected}\n".encode('utf-8') in process.stdout
+    assert process.returncode == 0
+
+    # the stack must end up empty
+    assert process.stdout.endswith(b"stack: []\n")
