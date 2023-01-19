1334dbf9d1f60380238afa7ea9b6d1e4912b78c6
xqb64 <x@xqb.ch>
2023-01-19 12:25:24 +0100

    add a comment for the ALLOC macro

--- src/util.h
+++ src/util.h
@@ -11,6 +11,11 @@ char *own_string_n(const char *string, int n);
 char *strcat_dynarray(DynArray_char_ptr array);
 char *read_file(const char *path);
 
+/* The purpose of the ALLOC macro is to take an
+ * existing object, put it on the heap and return
+ * a pointer to the newly allocated object.
+ * This is accomplished by (ab)using memcpy's
+ * return value which is the pointer to dest. */
 #define ALLOC(obj) (memcpy(malloc(sizeof((obj))), &(obj), sizeof((obj))))
 
 #endif
