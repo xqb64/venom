6a1a8e596634935ac9865ab059269a5a4bc37869
xqb64 <x@xqb.ch>
2023-01-20 05:50:21 +0100

    edit comment in dynarray.c

--- src/dynarray.c
+++ src/dynarray.c
@@ -3,20 +3,26 @@
 #include "dynarray.h"
 
 char *strcat_dynarray(DynArray_char_ptr array) {
-    /* Determine the combined length of all strings
-     * within the array, excluding the null terminators. */
+    /* Compute the combined length of all strings
+     * in the array, excluding the null terminat-
+     * ors after each of the strings in the array. */
     int msg_len = 0;
     for (size_t i = 0; i < array.count; i++) {
         msg_len += strlen(array.data[i]);
     }
 
-    /* We need to allocate sufficient space for the resulting string.
-     * That would be: msg_len + (array.count - 1) * 2 + 1, where:
-     * - (array.count - 1) * 2 is the length of (comma, space)
-     * pairs (", ") that come after each string except the last
-     * one (hence -1),
-     * - +1 in the end is for the null terminator of the result. */
+    /* Allocate sufficient space for the new str-
+     * ing: msg_len + (array.count - 1) * 2 + 1,
+     * where: (array.count - 1) * 2 is the length
+     * of ", " pairs that come after each string,
+     * except the last one (hence -1), and +1 in
+     * the end is for the null terminator of the
+     * resulting string. */
     char *msg = malloc(msg_len + (array.count - 1) * 2 + 1);
+    
+    /* Concatenate strings in the array into the
+     * previously allocated string, while taking
+     * care of not adding ", " after the last one. */
     for (size_t i = 0; i < array.count; i++) {
         strcat(msg, array.data[i]);
         if (i != array.count - 1) {
